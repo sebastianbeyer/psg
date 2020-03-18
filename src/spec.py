3b56ebefe766c68b60edbf02fd1e84e7b031d5aa
Sebastian <sebi@riseup.net>
2020-03-18 11:13:11 +0100

    add default possibility

--- src/spec.py
+++ src/spec.py
@@ -1,7 +1,7 @@
 from ruamel.yaml import YAML
+import json
 
 yaml = YAML(typ='safe')
-
 
 class Spec():
     """
@@ -10,17 +10,36 @@ class Spec():
     def __init__(self, name, fname):
         self.name = name
         self.data = self._load_specs(fname)
+        self.default = self._maybe_load_defaults()
+
+
+    def __str__(self):
+        return json.dumps(self.data, indent=4)
+
+
 
     def _load_specs(self, specfile):
         with open(specfile, 'r') as stream:
-            return yaml.load(stream)
+            data = yaml.load(stream)
+            #remove top level name
+            datareduced = [i[self.name] for i in data]
+            return datareduced
+
+    def _maybe_load_defaults(self):
+        for spec in self.data:
+            if spec['spec'] == 'default':
+                print('found default!')
+                return spec
+        return 'none'
 
     def findSpec(self, identifier):
         spec = next(
-            (item[self.name]
-             for item in self.data if item[self.name]['spec'] == identifier),
+            (item
+             for item in self.data if item['spec'] == identifier),
             None)
         if spec is None:
             raise KeyError("spec identifier {} not found".format(identifier))
         else:
             return spec
+
+
