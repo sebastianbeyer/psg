56d803cf87d5ac71a2256b446369ee1fc8976b3f
Sebastian <sebi@riseup.net>
2020-03-27 11:08:12 +0100

    make studies work

--- src/experiment.py
+++ src/experiment.py
@@ -22,9 +22,12 @@ class Experiment():
         self.version = version.git_version()
 
         output = {
-                'base': os.path.join(paths.exp_envs_path, self.spec, self.spec + '.nc', ),
-                'ts_file': os.path.join(paths.exp_envs_path, self.spec, 'ts_' + self.spec + '.nc', ),
-                'extra_file': os.path.join(paths.exp_envs_path, self.spec, 'ex_' + self.spec + '.nc', ),
+                'base': os.path.join(paths.exp_envs_path,
+                    self.spec, self.spec + '.nc', ),
+                'ts_file': os.path.join(paths.exp_envs_path,
+                    self.spec, 'ts_' + self.spec + '.nc', ),
+                'extra_file': os.path.join(paths.exp_envs_path,
+                    self.spec, 'ex_' + self.spec + '.nc', ),
                 'extra_vars': 'velsurf_mag,mask,thk,topg,usurf,climatic_mass_balance',
                 }
         self.basedata = {
@@ -69,6 +72,16 @@ class Experiment():
         return string
 
     def write_to_file(self, out_file, templateName):
+        # first check if parameter study:
+        isStudy, key, val = self._find_parameter_study()
+        if isStudy:
+            self._write_to_file_multi(out_file, templateName, key, val)
+        else:
+            self._write_to_file_single(out_file, templateName)
+
+
+    def _write_to_file_single(self, out_file, templateName):
+        """Write the usual single file (no parameter study)"""
         loader = jinja2.FileSystemLoader(searchpath=paths.templates_path)
         env = jinja2.Environment(loader=loader,
                                  trim_blocks=True,
@@ -78,6 +91,47 @@ class Experiment():
         template.stream(self.basedata).dump(out_file)
 
         self._append_options(out_file)
+
+    def _write_to_file_multi(self, out_file, templateName, key, val):
+        """Write multiple files (parameter study)"""
+        loader = jinja2.FileSystemLoader(searchpath=paths.templates_path)
+        env = jinja2.Environment(loader=loader,
+                                 trim_blocks=True,
+                                 undefined=jinja2.StrictUndefined)
+        template = env.get_template(templateName)
+
+        filename_no_ext, _ = os.path.splitext(out_file)
+        print(filename_no_ext)
+        for value in val:
+            print(value)
+            out_file_multi = filename_no_ext + "_" + key + "_" + str(value) + ".sh"
+            # print(out_file_multi)
+
+            # update
+            for submodel in [self.grid, self.time, self.icedyn, self.ocean, self.climate]:
+                for key2, value2 in submodel.items():
+                    if key2 == key:
+                        submodel[key2] = value
+                        print("found {}   -> setting it to {}".format(key2, value))
+            output = {
+                      'base': os.path.join(paths.exp_envs_path,
+                          self.spec,
+                          self.spec + "_" + key + "_" + str(value) + '.nc', ),
+                      'ts_file': os.path.join(paths.exp_envs_path,
+                          self.spec,
+                          'ts_' + self.spec + "_" + key + "_" + str(value) + '.nc', ),
+                      'extra_file': os.path.join(paths.exp_envs_path,
+                          self.spec,
+                          'ex_' + self.spec + "_" + key + "_" + str(value) + '.nc', ),
+                      'extra_vars': 'velsurf_mag,mask,thk,topg,usurf,climatic_mass_balance',
+                      }
+            self.basedata['output'] = output
+
+            # TODO: notice in file
+
+            template.stream(self.basedata).dump(out_file_multi)
+            self._append_options(out_file_multi)
+
 
     def _append_options(self, out_file):
         """
@@ -91,3 +145,14 @@ class Experiment():
                         line = "-{} {} \\".format(key, value)
                         # print(line)
                         f.write('  ' + line + '\n')
+
+    def _find_parameter_study(self):
+        """Find parameters that are lists. Currently only on is allowed at a time"""
+        for submodel in [self.grid, self.time, self.icedyn, self.ocean, self.climate]:
+            for key, value in submodel.items():
+                if isinstance(value, list):
+                    print("Found parameter study: {}: {}".format(key, value))
+                    return True, key, value
+        return False, "no-study", "no-study"
+
+
